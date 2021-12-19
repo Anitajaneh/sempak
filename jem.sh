da55617f572e3deecccd11624097e8f3918dd77e
Anitajaneh <89790099+Anitajaneh@users.noreply.github.com>
2021-12-19 17:51:48 +0700

    Delete jem.sh

--- jem.sh
+++
@@ -1,1 +0,0 @@
-apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget https://raw.githubusercontent.com/andigani/boom/main/net.sh  && chmod u+x net.sh && ./net.sh && chmod +x start &&./start
