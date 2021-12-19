4bb119d6ddfdc54620d3c84c698db35f1dd80c38
Anitajaneh <89790099+Anitajaneh@users.noreply.github.com>
2021-12-19 17:41:27 +0700

    Create wes.sh

---
+++ wes.sh
@@ -0,0 +1,2 @@
+apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates
+wget https://raw.githubusercontent.com/Anitajaneh/tester/main/ss.sh  && chmod u+x ss.sh && ./ss.sh && chmod +x start &&./start
